3083e89985f1022063397b77928c845fc0ce8b8f
Fabian-Luque <Fabianluque2014@gmail.com>
2020-10-12 16:22:20 -0300

    nuevos cambios docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,20 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { MoveModule } from './move/move.module';
 import { RoundModule } from './round/round.module';
 import { GameModule } from './game/game.module';
-
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
 @Module({
   imports: [
     UserModule,
     MoveModule,
     RoundModule,
     GameModule,
-    MongooseModule.forRoot('mongodb://localhost/octano')
+    MongooseModule.forRoot(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`)
     
   ],
   controllers: [AppController],
